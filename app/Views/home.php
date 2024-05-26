68b4f6cd5115572ddb8385d4a42020db4b45f2fb
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-05-26 17:27:41 +0530

    changes

--- app/Views/home.php
+++ app/Views/home.php
@@ -156,7 +156,7 @@
         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-        <iframe src="https://docs.google.com/viewer?url=https://admin.printbizz.in/writable/uploads/20240501/1714589422_ec47a3c6ede44c406349.pdf&embedded=true" style="width:100%; height:99%;" frameborder="0"></iframe>
+        <iframe src="https://docs.google.com/viewer?url=https://admin.printbizz.in/writable/uploads/20240501/1714580826_bd204792e8525971b462.pdf&embedded=true" style="width:100%; height:99%;" frameborder="0"></iframe>
 
         <!-- <embed src="https://admin.printbizz.in/writable/uploads/20240501/1714589422_ec47a3c6ede44c406349.pdf" id="new1" onclick="PrintElem(this.id)" style="width: 100%; height:99%;"> -->
     </div>
